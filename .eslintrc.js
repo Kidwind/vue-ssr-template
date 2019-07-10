9e0f596c61809bf28af03d2abb5cd440a96f6a2c
Kidwind <Kidwind@gmail.com>
2019-07-10 17:40:39 +0800

    Format.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        /* @xuhai start ESLint 配置 */
+        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'semi': ['error', 'always']
+        /* @xuhai end */
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+};
